b80e364da42a40258d879c3229a08a4981c6e124
aissa <106400398+AissatouGassama@users.noreply.github.com>
2026-08-30 11:09:24 +0200

    Maj identifiants test_login apres expiration mot de passe

--- Automation/test_login.py
+++ Automation/test_login.py
@@ -2,5 +2,5 @@ from Pages.login_page import LoginPage
 
 def test_login_reussi(page):
     login_page = LoginPage(page)
-    login_page.se_connecter("mngr664973", "jenuzap")
+    login_page.se_connecter("mngr665671", "vunyvAz")
     assert "Manager" in page.locator("body").inner_text()
